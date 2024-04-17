6a0f96689a3f1318660bfb17bec05f81060d7bff
saarthak24 <saarthaksethi2008@gmail.com>
2024-04-16 20:48:02 -0400

    Started working on XML for the chessboard

--- Project3_ssethi20_anikku/app/src/main/java/edu/gmu/project3_ssethi20_anikku/play_friend.java
+++ Project3_ssethi20_anikku/app/src/main/java/edu/gmu/project3_ssethi20_anikku/play_friend.java
@@ -3,10 +3,12 @@ package edu.gmu.project3_ssethi20_anikku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-public class MainActivity extends AppCompatActivity {
+
+public class play_friend extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_play_friend);
     }
 }
