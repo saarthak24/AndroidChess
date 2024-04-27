b1a766a36bbb33bae81cd618abbd3ebbe2fcd002
anudeepnikku <38710116+anudeepnikku@users.noreply.github.com>
2024-04-27 13:13:54 -0400

    Selected piece did not move to destination square
    There was a logic error that caused the selected piece to not move to a destination square if one of the rows and columns were the same (Ex: pawn from e2 to e4). The logic for that code has been corrected. Added functionality to all grids clicked on the chessboard (since the onClick() listener wasn't fully implemented, clicking on some items on the grid with a missing onClick() listener method would cause the entire app to crash, this has been fixed by adding all onClick() listeners to all ImageView grid items in the PlayComputer.java file

--- Project3_ssethi20_anikku/app/src/main/java/edu/gmu/project3_ssethi20_anikku/PlayComputer.java
+++ Project3_ssethi20_anikku/app/src/main/java/edu/gmu/project3_ssethi20_anikku/PlayComputer.java
@@ -77,6 +77,7 @@ public class PlayComputer extends AppCompatActivity
 
     public void selectChessPiece(int row, int col)
     {
+        System.out.println("The chess piece selected: " + chessPieceSelected());
         if(chessPieceSelected()==false && computerTurn==false && chessGrid[row][col]==BLANK_SQUARE)
         {
             Toast.makeText(getApplicationContext(), "Please select a chess piece", Toast.LENGTH_SHORT).show();
@@ -100,7 +101,7 @@ public class PlayComputer extends AppCompatActivity
     {
         System.out.println("Chess status: " + chessPieceSelected());
         System.out.println("selected row: " + selectedPieceRow + ", col: " + selectedPieceCol);
-        if(chessPieceSelected==true)
+        if(chessPieceSelected==true && (row!=selectedPieceRow || col!=selectedPieceCol))
         {
             System.out.println("here2dest");
             destinationTileRow=row;
@@ -795,58 +796,406 @@ public class PlayComputer extends AppCompatActivity
         addWhiteQueen(7,3);
         addWhiteKing(7,4);
         spanPawns(WHITE, 6);
+
+        selectedPieceRow=-1;
+        selectedPieceCol=-1;
+        destinationTileRow=-1;
+        destinationTileCol=-1;
     }
 
-    public void a8Clicked(View view) {
-        System.out.println("tile 1 has been clicked");
-        selectChessPiece(0,0);
+    //checking first row of chessboard (last row in array layout)
+    public void a1Clicked(View view)
+    {
+        selectChessPiece(7, 0);
+        selectDestinationLocation(7,0);
     }
 
+    public void b1Clicked(View view)
+    {
+        selectChessPiece(7,1);
+        selectDestinationLocation(7,1);
+    }
+
+    public void c1Clicked(View view)
+    {
+        selectChessPiece(7,2);
+        selectDestinationLocation(7,2);
+    }
+
+    public void d1Clicked(View view)
+    {
+        selectChessPiece(7,3);
+        selectDestinationLocation(7,3);
+    }
+
+    public void e1Clicked(View view)
+    {
+        selectChessPiece(7,4);
+        selectDestinationLocation(7,4);
+    }
+
+    public void f1Clicked(View view)
+    {
+        selectChessPiece(7,5);
+        selectDestinationLocation(7,5);
+    }
+
+    public void g1Clicked(View view)
+    {
+        selectChessPiece(7,6);
+        selectDestinationLocation(7,6);
+    }
+
+    public void h1Clicked(View view)
+    {
+        selectChessPiece(7,7);
+        selectDestinationLocation(7,7);
+    }
+
+
+    //checking second row of chessboard (second to last row in array layout)
     public void a2Clicked(View view)
     {
         selectChessPiece(6, 0);
+        selectDestinationLocation(6,0);
         System.out.println("White kingside pawn clicked");
+
     }
 
     public void b2Clicked(View view)
     {
         selectChessPiece(6, 1);
+        selectDestinationLocation(6,1);
     }
 
     public void c2Clicked(View view)
     {
         selectChessPiece(6, 2);
+        selectDestinationLocation(6,2);
     }
 
     public void d2Clicked(View view)
     {
         selectChessPiece(6, 3);
+        selectDestinationLocation(6,3);
     }
 
     public void e2Clicked(View view)
     {
         selectChessPiece(6, 4);
+        selectDestinationLocation(6,4);
     }
 
     public void f2Clicked(View view)
     {
         selectChessPiece(6, 5);
+        selectDestinationLocation(6,5);
     }
 
     public void g2Clicked(View view)
     {
         selectChessPiece(6, 6);
+        selectDestinationLocation(6,6);
     }
 
     public void h2Clicked(View view)
     {
         selectChessPiece(6, 7);
+        selectDestinationLocation(6,7);
+    }
+
+    //checking third row of chess board
+    public void a3Clicked(View view)
+    {
+        selectChessPiece(5, 0);
+        selectDestinationLocation(5, 0);
+    }
+
+    public void b3Clicked(View view)
+    {
+        selectChessPiece(5, 1);
+        selectDestinationLocation(5, 1);
+    }
+
+    public void c3Clicked(View view)
+    {
+        selectChessPiece(5, 2);
+        selectDestinationLocation(5, 2);
+    }
+
+    public void d3Clicked(View view)
+    {
+        selectChessPiece(5, 3);
+        selectDestinationLocation(5, 3);
+    }
+
+    public void e3Clicked(View view)
+    {
+        selectChessPiece(5, 4);
+        selectDestinationLocation(5, 4);
+    }
+
+    public void f3Clicked(View view)
+    {
+        selectChessPiece(5, 5);
+        selectDestinationLocation(5, 5);
+    }
+
+    public void g3Clicked(View view)
+    {
+        selectChessPiece(5, 6);
+        selectDestinationLocation(5, 6);
+    }
+
+    public void h3Clicked(View view)
+    {
+        selectChessPiece(5, 7);
+        selectDestinationLocation(5, 7);
+    }
+
+    //checking fourth row of chess board
+    public void a4Clicked(View view)
+    {
+        selectChessPiece(4,0);
+        selectDestinationLocation(4, 0);
+    }
+
+    public void b4Clicked(View view)
+    {
+        selectChessPiece(4,1);
+        selectDestinationLocation(4,1);
+    }
+
+    public void c4Clicked(View view)
+    {
+        selectChessPiece(4,2);
+        selectDestinationLocation(4,2);
+    }
+
+    public void d4Clicked(View view)
+    {
+        selectChessPiece(4,3);
+        selectDestinationLocation(4,3);
     }
 
     public void e4Clicked(View view)
     {
         selectChessPiece(4,4);
         selectDestinationLocation(4,4);
+    }
+
+    public void f4Clicked(View view)
+    {
+        selectChessPiece(4,5);
+        selectDestinationLocation(4,5);
+    }
+
+    public void g4Clicked(View view)
+    {
+        selectChessPiece(4,6);
+        selectDestinationLocation(4,6);
+    }
+
+    public void h4Clicked(View view)
+    {
+        selectChessPiece(4,7);
+        selectDestinationLocation(4,7);
+    }
+
+    //checking the fifth row of the chessboard
+    public void a5Clicked(View view)
+    {
+        selectChessPiece(3,0);
+        selectDestinationLocation(3, 0);
+    }
+
+    public void b5Clicked(View view)
+    {
+        selectChessPiece(3,1);
+        selectDestinationLocation(3, 1);
+    }
+
+    public void c5Clicked(View view)
+    {
+        selectChessPiece(3,2);
+        selectDestinationLocation(3, 2);
+    }
+
+    public void d5Clicked(View view)
+    {
+        selectChessPiece(3,3);
+        selectDestinationLocation(3, 3);
+    }
+
+    public void e5Clicked(View view)
+    {
+        selectChessPiece(3,4);
+        selectDestinationLocation(3, 4);
+    }
+
+    public void f5Clicked(View view)
+    {
+        selectChessPiece(3,5);
+        selectDestinationLocation(3, 5);
+    }
+
+    public void g5Clicked(View view)
+    {
+        selectChessPiece(3,6);
+        selectDestinationLocation(3, 6);
+    }
+
+    public void h5Clicked(View view)
+    {
+        selectChessPiece(3,7);
+        selectDestinationLocation(3, 7);
+    }
+
+    //checking the sixth row of the chess board
+    public void a6Clicked(View view)
+    {
+        selectChessPiece(2,0);
+        selectDestinationLocation(2, 0);
+    }
+
+    public void b6Clicked(View view)
+    {
+        selectChessPiece(2,1);
+        selectDestinationLocation(2, 1);
+    }
+
+    public void c6Clicked(View view)
+    {
+        selectChessPiece(2,2);
+        selectDestinationLocation(2, 2);
+    }
+
+    public void d6Clicked(View view)
+    {
+        selectChessPiece(2,3);
+        selectDestinationLocation(2, 3);
+    }
+
+    public void e6Clicked(View view)
+    {
+        selectChessPiece(2,4);
+        selectDestinationLocation(2, 4);
+    }
+
+    public void f6Clicked(View view)
+    {
+        selectChessPiece(2,5);
+        selectDestinationLocation(2, 5);
+    }
+
+    public void g6Clicked(View view)
+    {
+        selectChessPiece(2,6);
+        selectDestinationLocation(2, 6);
+    }
+
+    public void h6Clicked(View view)
+    {
+        selectChessPiece(2,7);
+        selectDestinationLocation(2, 7);
+    }
+
+    //checking the 7th row of the chessboard (which is the second to first row in the array representation)
+    public void a7Clicked(View view)
+    {
+        selectChessPiece(1, 0);
+        selectDestinationLocation(1, 0);
+    }
+
+    public void b7Clicked(View view)
+    {
+        selectChessPiece(1, 1);
+        selectDestinationLocation(1, 1);
+    }
+
+    public void c7Clicked(View view)
+    {
+        selectChessPiece(1, 2);
+        selectDestinationLocation(1, 2);
+    }
+
+    public void d7Clicked(View view)
+    {
+        selectChessPiece(1, 3);
+        selectDestinationLocation(1, 3);
+    }
+
+    public void e7Clicked(View view)
+    {
+        selectChessPiece(1, 4);
+        selectDestinationLocation(1, 4);
+    }
+
+    public void f7Clicked(View view)
+    {
+        selectChessPiece(1, 5);
+        selectDestinationLocation(1, 5);
+    }
+
+    public void g7Clicked(View view)
+    {
+        selectChessPiece(1, 6);
+        selectDestinationLocation(1, 6);
+    }
+
+    public void h7Clicked(View view)
+    {
+        selectChessPiece(1, 7);
+        selectDestinationLocation(1, 7);
+    }
+
+    //checking the 8th row of the chessboard, which is the first row in the array representation)
+    public void a8Clicked(View view)
+    {
+        selectChessPiece(0,0);
+        selectDestinationLocation(0,0);
+    }
+
+    public void b8Clicked(View view)
+    {
+        selectChessPiece(0,1);
+        selectDestinationLocation(0,1);
+    }
+
+    public void c8Clicked(View view)
+    {
+        selectChessPiece(0,2);
+        selectDestinationLocation(0,2);
+    }
+
+    public void d8Clicked(View view)
+    {
+        selectChessPiece(0,3);
+        selectDestinationLocation(0,3);
+    }
+
+    public void e8Clicked(View view)
+    {
+        selectChessPiece(0,4);
+        selectDestinationLocation(0,4);
+    }
+
+    public void f8Clicked(View view)
+    {
+        selectChessPiece(0,5);
+        selectDestinationLocation(0,5);
+    }
+
+    public void g8Clicked(View view)
+    {
+        selectChessPiece(0,6);
+        selectDestinationLocation(0,6);
+    }
+
+    public void h8Clicked(View view)
+    {
+        selectChessPiece(0,7);
+        selectDestinationLocation(0,7);
     }
 
     public void clearSelection(View view)
